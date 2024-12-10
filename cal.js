03a06aadfd0ec2b40159539fa40ecd7ea39f6ee3
vanessaleivis7 <vanessalievis7@gmail.com>
2024-12-10 07:25:31 -0500

    calculadora

---
+++ cal.js
@@ -0,0 +1,17 @@
+function numero(value) {
+    const display = document.getElementById('display');
+    display.value += value; //muestra en pantalla
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) { 
+        display.value = 'Error';
+    }
+}
